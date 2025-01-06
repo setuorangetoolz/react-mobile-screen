73dfe9ac20ecd8c02a1e271c5e9743b6fa3f2fdc
setu <setu@orangetoolz.com>
2025-01-06 22:12:20 +0600

    module css issue fix

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -17,9 +17,7 @@ export default [
     plugins: [
       resolve(),
       postcss({
-        modules: {
-          generateScopedName: "[name]__[local]___[hash:base64:5]",
-        },
+        modules: true,
         extract: "dist/styles.css",
         use: ["sass"],
       }),
